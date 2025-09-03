5787047aedac38075e43575c77d390f559750b2d
dcuevas <dario.cuevas@email.com>
2025-09-04 00:12:26 +0200

    chore: update comment to force vercel deployment on push to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
             <footer className="row-start-3 mt-12 text-sm text-zinc-500">
                 © {new Date().getFullYear()} {profile.name}
             </footer>
-            <SpeedInsights /> # Vercel stats
+            <SpeedInsights /> # Vercel stats plugin
         </div>
     );
 }
